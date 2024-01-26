0039b3bfc7ec3fc21fe4992bc8b982d4a1b13ffa
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-26 23:47:40 +0530

    indentation for jwtwebtoken changed

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  JWT_SECRET: "my_secret_key",
-};
+const JWT_SECRET = "anishssecretkey";
 
+module.exports = {
+  JWT_SECRET,
+};
 //this file is used to store the secret key for the jwt token.
 //it is used in Authentication.
