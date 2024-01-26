f256189e7e4753217c037b1f3511afbe5e4b9cd0
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-26 23:59:19 +0530

    added the users via routing

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,38 @@
 //1. define a new router
 const express = require("express");
+const { User } = require("../db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
 const zod = require("zod");
+
 const router = express.Router();
 module.exports = router;
+
+const signupBody = zod.object({
+    username : zod.string().email(),
+    fistName : zod.string(),
+    lastName : zod.string(),
+    password : zod.string()
+})
+
+router.post("/signup", asyn (req, res)) => {
+    const { success } = signupBody.safeParse(req.body)
+    if(!success){
+        return res.status(411).json({
+            message : " Email already taken / Incorrect / Invalid Email!"
+        })
+    }
+    const existingUser =  await User.findOne({
+        username : req.body.username
+    })
+}
 
 //2. import it in the index.js file.
 
 //3. adding 3 new routes in the user_routes
 //sigup route --> input validation is needed to be done via zodf
+
+//connection with the database as well.
 
 router.post("/signup", (req, res) => {
   res.send({
