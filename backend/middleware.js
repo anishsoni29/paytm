b0d02d8368087c7c302342984a03abd14fdbc9df
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-27 01:23:41 +0530

    added the middleware accordingly

--- backend/middleware.js
+++ backend/middleware.js
@@ -2,3 +2,34 @@
 //2. Verifies the token is valid or not
 //3.Puts the userId in the req object
 //4. Return 403 if not in req object.
+
+//middleware with auth_header function:
+const { JWT_SECRET } = require("./config");
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || authHeader.startsWith("Bearer")) {
+    return res.status(403).json({});
+  }
+
+  //selecting the second part of the authHeader and then decoding it to verify the authorization.
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    req.userId = decoded.userId;
+    next();
+  } catch {
+    err;
+  }
+  {
+    return res.status(403).json({});
+  }
+};
+
+module.exports = {
+  authMiddleware,
+};
