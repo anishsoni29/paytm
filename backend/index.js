cec2e84f7246262c07751fe800f82da371a833ff
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-26 23:09:14 +0530

    added the comments for routing

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-
+const mainRouter = require("./routes/index");
 const app = express();
 
-app.use("/api/v1");
+app.use("/api/v1", mainRouter);
+//we are bootstraping our application as such
+//the requests which we send on the /api/v1 route
+//will go to the mainRouter which takes the data
+//from the routes folder.
+
 //app.use is a middleware.
 // /api/v1 --> all reqs will start with this.
