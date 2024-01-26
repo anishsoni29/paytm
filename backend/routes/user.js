2ec14887b1d718fc47ea3124571ba851f9e2ddcc
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-26 23:53:50 +0530

    added input validation usind zod

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,20 @@
 //1. define a new router
 const express = require("express");
+const zod = require("zod");
 const router = express.Router();
 module.exports = router;
 
 //2. import it in the index.js file.
+
+//3. adding 3 new routes in the user_routes
+//sigup route --> input validation is needed to be done via zodf
+
+router.post("/signup", (req, res) => {
+  res.send({
+    username: "name@gmail.com",
+    firstName: "name",
+    lastName: "name",
+    password: "12345678",
+  });
+  res.status(200);
+});
