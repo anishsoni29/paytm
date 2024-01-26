231f35de026f5d72c5bf7f52009a8dcec8d4f32b
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-27 00:20:14 +0530

    added the signup route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,38 +9,54 @@ const router = express.Router();
 module.exports = router;
 
 const signupBody = zod.object({
-    username : zod.string().email(),
-    fistName : zod.string(),
-    lastName : zod.string(),
-    password : zod.string()
-})
+  username: zod.string().email(),
+  fistName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
 
 //using the async await function
-router.post("/signup", async (req, res)) => {
-    const { success } = signupBody.safeParse(req.body)
-    if(!success){
-        return res.status(411).json({
-            message : " Email already taken / Incorrect / Invalid Email!"
-        })
-    }
-    const existingUser =  await User.findOne({
-        username : req.body.username
-    })
+router.post("/signup", async function (req, res) {
+  const { success } = signupBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken/ Incorrect / Invalid Email!",
+    });
+  }
+});
+
+const existingUser = await User.findOne({
+  username: req.body.username,
+});
+
+if (existingUser) {
+  return res.status(411).json({
+    message: " Email already taken / Incorrect / Invalid Email!",
+  });
 }
+
+const user = await User.create({
+  username: req.body.username,
+  password: req.body.password,
+  firstName: req.body.firstName,
+  lastName: req.body.lastName,
+});
+
+const UserId = user._id;
+
+const token = jwt.sign(
+  {
+    UserId,
+  },
+  JWT_SECRET
+);
+
+res.json({
+  message: "User Created Successfully!",
+  token: token,
+});
 
 //2. import it in the index.js file.
 
 //3. adding 3 new routes in the user_routes
 //sigup route --> input validation is needed to be done via zodf
-
-//connection with the database as well.
-
-router.post("/signup", (req, res) => {
-  res.send({
-    username: "name@gmail.com",
-    firstName: "name",
-    lastName: "name",
-    password: "12345678",
-  });
-  res.status(200);
-});
