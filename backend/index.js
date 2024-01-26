c780e9e91778f7c169a785660e6bbf8e2e7b5d25
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-26 23:20:13 +0530

    added the cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
+const cors = require("cors");
 const express = require("express");
 const mainRouter = require("./routes/index");
 const app = express();
 
+app.use(cors());
 app.use("/api/v1", mainRouter);
+
+module.exports = router;
+
 //we are bootstraping our application as such
 //the requests which we send on the /api/v1 route
 //will go to the mainRouter which takes the data
@@ -10,3 +15,5 @@ app.use("/api/v1", mainRouter);
 
 //app.use is a middleware.
 // /api/v1 --> all reqs will start with this.
+
+//adding the cors middlewares
