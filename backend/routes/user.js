21c6b7c2507b437a5bc5b86119ced8efb6480bf6
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-27 00:42:51 +0530

    added the routes for signin and signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,95 +1,93 @@
-//1. define a new router
+// backend/routes/user.js
 const express = require("express");
+
+const router = express.Router();
+const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
-const zod = require("zod");
 
-const router = express.Router();
-module.exports = router;
+const signupBody = zod.object({
+  username: zod.string().email(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
 
-//defining the signin route
+router.post("/signup", async (req, res) => {
+  const { success } = signupBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+
+  const existingUser = await User.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already taken/Incorrect inputs",
+    });
+  }
+
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+  const userId = user._id;
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    message: "User created successfully",
+    token: token,
+  });
+});
+
 const signinBody = zod.object({
   username: zod.string().email(),
   password: zod.string(),
 });
 
-//adding the router for the signin above
-router.post("/signin", async function(req,res){
-    const { success } = signinBody.safeParse(req.body);
-    if (!success) {
-        return res.status(411).json({
-            message : "Incorrect inputs"
-        })
-    }
-})
-
-
-//defining the signup route
-const signupBody = zod.object({
-  username: zod.string().email(),
-  fistName: zod.string(),
-  lastName: zod.string(),
-  password: zod.string(),
-});
-
-//using the async await function
-//router for the signup
-router.post("/signup", async function (req, res) {
-  const { success } = signupBody.safeParse(req.body);
+router.post("/signin", async (req, res) => {
+  const { success } = signinBody.safeParse(req.body);
   if (!success) {
     return res.status(411).json({
-      message: "Email already taken/ Incorrect / Invalid Email!",
+      message: "Email already taken / Incorrect inputs",
     });
   }
-});
 
-const existingUser = await User.findOne({
-  username: req.body.username,
-});
-
-if (existingUser) {
-  return res.status(411).json({
-    message: " Email already taken / Incorrect / Invalid Email!",
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
   });
-}
 
-const user = await User.create({
-  username: req.body.username,
-  password: req.body.password,
-  firstName: req.body.firstName,
-  lastName: req.body.lastName,
-});
-
-is (user) {
-    const token = jwt.sign({
-        userId : user._id
-    }, JWT_SECRET);
+  if (user) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      JWT_SECRET
+    );
 
     res.json({
-        token : token,
-    })
+      token: token,
+    });
     return;
-}
-res.status(411).json({
-    message : "Error while signin in!"
-})
+  }
 
-const UserId = user._id;
-
-const token = jwt.sign(
-  {
-    UserId,
-  },
-  JWT_SECRET
-);
-
-res.json({
-  message: "User Created Successfully!",
-  token: token,
+  res.status(411).json({
+    message: "Error while logging in",
+  });
 });
 
-//2. import it in the index.js file.
-
-//3. adding 3 new routes in the user_routes
-//sigup route --> input validation is needed to be done via zodf
+module.exports = router;
