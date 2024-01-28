8ab5490e7ad78f65138e48f7fa15ab389ca7770d
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-29 02:29:40 +0530

    added the balance endpoint for users to get their balances

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const userRouter = require("./user");
-const accountRouter = require("./accounts");
+const accountRouter = require("./account");
+const { authMiddleware } = require("../middleware");
 
 const router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/accounts", accountRouter);
+
+//an endpoint for users to get their balance
+router.get("/balance", authMiddleware, async (req, res) => {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+  res.json({
+    balance: account.balance,
+  });
+});
 
 module.exports = router;
