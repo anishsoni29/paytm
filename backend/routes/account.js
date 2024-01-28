08881cf151256319c8d3f523e9ee968e442f556e
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-29 02:39:57 +0530

    added the balance fetch code in the accounts file

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,7 +1,21 @@
 //creating a new router here and exporting it
 
 const express = require("express");
+const { authMiddleware } = require("../middleware");
+const { Account } = require("../db");
 
 const router = express.Router();
+
+//an endpoint for users to get their balance
+router.get("/balance", authMiddleware, async (req, res) => {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+  res.json({
+    balance: account.balance,
+  });
+});
+
+//an endpoint for users to transfer money
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,14 +8,4 @@ const router = express.Router();
 router.use("/user", userRouter);
 router.use("/accounts", accountRouter);
 
-//an endpoint for users to get their balance
-router.get("/balance", authMiddleware, async (req, res) => {
-  const account = await Account.findOne({
-    userId: req.userId,
-  });
-  res.json({
-    balance: account.balance,
-  });
-});
-
 module.exports = router;
