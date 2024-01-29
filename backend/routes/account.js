494fc520e2546485b2fad34011c30e3277d9df05
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-29 15:55:41 +0530

    created the Balance and transfer endpoints

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -16,6 +16,53 @@ router.get("/balance", authMiddleware, async (req, res) => {
   });
 });
 
-//an endpoint for users to transfer money
+//an endpoint for users to transfer money to another account
+//there should be a transfer session
+
+router
+  .post("/transfer", authMiddleware, async (req, res) => {
+    //start a session for transaction, + , -
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    //Fetch the accounts within the transactions
+    const account = await Account.findOne({
+      userId: req.userId,
+    }).session(session);
+
+    if (!account || account.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: "Insufficient balance",
+      });
+    }
+
+    const toAccount = await Account.findOne({
+      userId: to,
+    }).session(session);
+
+    if (!toAccount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: " Invalid Account",
+      });
+    }
+
+    //perform the transfer
+    await Account.updateOne(
+      { userId: req.usedId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } });
+  })
+  .session(session);
+
+//commit the transaction
+await session.commitTransaction();
+res.json({
+  message: "Transaction Successful",
+});
 
 module.exports = router;
